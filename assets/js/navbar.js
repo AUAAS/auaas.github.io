219364dd34b2a3ae6fcf922d19c050000eb4dadb
Raymond Morland <30455116+RayMorland@users.noreply.github.com>
2025-08-23 08:48:33 -0700

    Fixed Navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,16 @@
-document.addEventListener("scroll", function () {
+function addScrolledClass() {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
     }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    addScrolledClass()
+});
+
+document.addEventListener("scroll", function () {
+    addScrolledClass()
 });
